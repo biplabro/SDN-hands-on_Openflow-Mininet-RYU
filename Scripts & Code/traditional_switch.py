cf4138255eebe33a93b94a06388b29e6400e3645
Biplab Roy <roy_biplab_89@yahoo.co.in>
2020-11-02 01:17:05 +0530

    script

---
+++ Scripts & Code/traditional_switch.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+
+"""Traditional Topology
+
+Run:
+sudo python traditional_switch.py
+
+Testing:
+sudo ovs-vsctl show
+sudo ovs-appctl fdb/show s1
+sudo ovs-ofctl -O OpenFlow13 dump-flows s1
+
+"""
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.cli import CLI
+from mininet.node import OVSSwitch, Controller, RemoteController
+
+class SingleSwitchTopo(Topo):
+    "Single switch connected to n hosts."
+    def build(self):
+        s1 = self.addSwitch('s1', failMode='standalone')
+        h1 = self.addHost('h1', mac="00:00:00:00:00:01", ip="192.168.1.1/24")
+        h2 = self.addHost('h2', mac="00:00:00:00:00:02", ip="192.168.1.2/24")
+        h3 = self.addHost('h3', mac="00:00:00:00:00:03", ip="192.168.1.3/24")
+        h4 = self.addHost('h4', mac="00:00:00:00:00:04", ip="192.168.1.4/24")
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s1)
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo = SingleSwitchTopo()
+    c1 = RemoteController('c1', ip='127.0.0.1')
+    net = Mininet(topo=topo, controller=c1)
+    net.start()
+    #net.pingAll()
+    CLI(net)
+    net.stop()
